9abefd749c0f41669da860d2fc22f5a6742b4eb0
mdarmankhan6252 <mdarmankhan6252@gmail.com>
2025-06-25 01:57:23 +0600

    I have initialized the redux ts project

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+
+      <h1>Redux With TS</h1>
+
+      <div>
+        <button>Increment</button>
+        <h1>0</h1>
+        <button>Decrement</button>
+      </div>
+
+    </>
+  )
+}
+
+export default App
