177f4ab7cb08a132d7aa8b270d3838f7ce9ab435
mdarmankhan6252 <mdarmankhan6252@gmail.com>
2025-06-25 02:31:49 +0600

    I have finished the problems

--- src/App.tsx
+++ src/App.tsx
@@ -8,11 +8,11 @@ function App() {
   const dispatch = useDispatch();
   const { count } = useSelector((state: RootState) => state.counter)
 
-  const handleIncrement = () => {
-    dispatch(increment())
+  const handleIncrement = (amount: number) => {
+    dispatch(increment(amount))
   }
-  const handleDecrement = () => {
-    dispatch(decrement())
+  const handleDecrement = (amount: number) => {
+    dispatch(decrement(amount))
   }
   const handleReset = () => {
     dispatch(reset())
@@ -24,9 +24,9 @@ function App() {
       <h1>Redux With TS</h1>
 
       <div>
-        <button onClick={handleIncrement}>Increment</button>
+        <button onClick={() => handleIncrement(10)}>Increment</button>
         <h1>{count}</h1>
-        <button onClick={handleDecrement}>Decrement</button>
+        <button onClick={() => handleDecrement(10)}>Decrement</button>
         <button onClick={handleReset}>Reset</button>
       </div>
 
--- src/redux/features/counterSlice.ts
+++ src/redux/features/counterSlice.ts
@@ -9,11 +9,12 @@ const counterSlice = createSlice({
    name: 'counter',
    initialState,
    reducers: {
-      increment: (state) => {
-         state.count = state.count + 1
+      increment: (state, action) => {
+         console.log(action);
+         state.count = state.count + action.payload
       },
-      decrement: (state) => {
-         state.count = state.count - 1
+      decrement: (state, action) => {
+         state.count = state.count - action.payload
       },
       reset: (state) => {
          state.count = 0
